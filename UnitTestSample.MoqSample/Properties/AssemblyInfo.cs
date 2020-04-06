a356b65dbd7832edfb803873dc2ebbe41d3aedb6
yTsunomoto <wasiwasimaru@gmail.com>
2020-04-07 00:09:35 +0900

    update Moqでref変数をテストするサンプルを追加。

---
+++ UnitTestSample.MoqSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSample.MoqSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSample.MoqSample")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84109418-febf-4ac7-946f-d0b52eeb203d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestSample.MoqSample/UnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestSample.MoqSample
+{
+    /// <summary>
+    /// テスト対象となるインターフェースです
+    /// </summary>
+    public interface ITestTarget
+    {
+        void DoSomething(string value1, ref string value2);
+    }
+
+    /// <summary>
+    /// コールバック用のデリデートを定義します
+    /// </summary>
+    /// <param name="value1"></param>
+    /// <param name="value2"></param>
+    delegate void DoSomethingCallback(string value1, ref string value2);
+
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            // Arrange
+            var mock = new Mock<ITestTarget>();
+            // モックの振る舞いを登録します
+            // ref参照の変数で値を変更します
+            mock.Setup(_ => _.DoSomething(It.IsAny<string>(), ref It.Ref<string>.IsAny))
+                .Callback(new DoSomethingCallback((string value1, ref string value2) => value2 = "This is output value."));
+            var target = mock.Object;
+
+            // Act
+            var input = "This is input value.";
+            target.DoSomething("", ref input);
+
+            // Assert
+            Assert.AreEqual("This is output value.", input);
+        }
+    }
+}
